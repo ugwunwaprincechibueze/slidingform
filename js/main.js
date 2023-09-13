13e36fb9e805d94e0c1db0814f963b14e6e2cd2a
ugwunwaprincechibueze <ugwunwachibueze70@gmail.com>
2023-09-13 20:30:43 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,12 @@
+
+document.querySelector(".sign-in-options-back-btn").addEventListener("click", ()=>{
+    document.querySelector("#form2").style.left = "150%"
+    console.log("Hello");
+})
+
+document.querySelector(".sign-in-options").addEventListener("click", ()=>{
+    document.querySelector("#form2").style.left = "50%"
+    console.log("Hiiii");
+})
+
+console.log(document.querySelector(".sign-in-options-back-btn"));
